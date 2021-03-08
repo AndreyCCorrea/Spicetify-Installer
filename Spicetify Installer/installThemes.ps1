ca125aa2a5284eed932ef8f87288b9c3f0e65a9e
AndreyCCorrea <67271721+AndreyCCorrea@users.noreply.github.com>
2021-03-08 17:12:13 -0300

    Update installThemes.ps1

--- Spicetify Installer/installThemes.ps1
+++ Spicetify Installer/installThemes.ps1
@@ -1,1 +1,32 @@
-Olá
+######################################################################
+#                                                                    #
+# Download and Unzip GitHub Repository                               #
+# Author: Sandro Pereira                                             #
+#                                                                    #
+######################################################################
+
+function DownloadGitHubRepository 
+{ 
+    $name = "Themes-Extensions"
+    $location = "$env:USERPROFILE"
+    
+    # Force to create a zip file 
+    $ZipFile = "$location\$Name.zip" 
+    New-Item $ZipFile -ItemType File -Force
+
+    $RepositoryZipUrl = "https://github.com/AndreyCCorrea/PowerShell-Scripts/blob/main/Spicetify%20Installer/Theme-Extensions.zip"
+    #$RepositoryZipUrl = "https://api.github.com/repos/$Author/$Name/zipball/$Branch"  
+    # download the zip 
+    Write-Host 'Starting downloading the GitHub Repository'
+    Invoke-RestMethod -Uri $RepositoryZipUrl -OutFile $ZipFile
+    Write-Host 'Download finished'
+
+    #Extract Zip File
+    Write-Host 'Starting unziping the GitHub Repository locally'
+    Expand-Archive -Path $ZipFile -DestinationPath $location -Force
+    Write-Host 'Unzip finished'
+    
+    # remove zip file
+    Remove-Item -Path $ZipFile -Force 
+}
+DownloadGithubRepository
